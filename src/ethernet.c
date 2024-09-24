d4ca2bbbfd1951b6b6133f7fb8861cb59902bbe3
ar7eniyan <ar7eniy.k@gmail.com>
2024-09-24 14:52:12 +0300

    feat: setup gpio for interfacing with lan8720a phy via rmii

--- src/ethernet.c
+++ src/ethernet.c
@@ -42,15 +42,48 @@ void setup_eth_dma(void)
 
 void setup_eth_gpio(void)
 {
-    // RMII pin configuration
-    SET_RCC_xxxxEN(RCC->AHB4ENR, RCC_AHB4ENR_GPIOAEN);
+    // RMII pin configuration:
+    // I: REF_CLK (PA1), CRS_DV (PA7), RXD0 (PC4), RXD1 (PC5)
+    // O: MDC (PC1), TX_EN (PB11), TXD0 (PB12), TXD1 (PB13)
+    // I/O: MDIO (PA2)
+    // All pins are using AF11.
+    // MDC and MDIO are working on ~2.5 MHz (OSPEED = 0b00), TXD0 and TXD1
+    // are working on 50 MHz (OSPEED = 0b10) and TX_EN is not high-speed
+    SET_RCC_xxxxEN(RCC->AHB4ENR,
+        RCC_AHB4ENR_GPIOAEN | RCC_AHB4ENR_GPIOBEN | RCC_AHB4ENR_GPIOCEN);
 
-    // Mode = 0b10 (Alternate Function)
-    MODIFY_REG(GPIOA->MODER, GPIO_MODER_MODE1, 0b10 << GPIO_MODER_MODE1_Pos);
-    // AF11
-    MODIFY_REG(GPIOA->AFR[0], GPIO_AFRL_AFSEL1, 0b1011 << GPIO_AFRL_AFSEL1_Pos);
-    // Output speed = 0b11 (Very High)
-    MODIFY_REG(GPIOA->OSPEEDR, GPIO_OSPEEDR_OSPEED1, 0b11 << GPIO_OSPEEDR_OSPEED1_Pos);
+    // MODER = AF, AFR = AF11 for REF_CLK, MDIO and CRS_DV
+    MODIFY_REG(GPIOA->MODER,
+        GPIO_MODER_MODE1 | GPIO_MODER_MODE2 | GPIO_MODER_MODE7,
+        (0b10 << GPIO_MODER_MODE1_Pos) | (0b10 << GPIO_MODER_MODE2_Pos) |
+        (0b10 << GPIO_MODER_MODE7_Pos));
+    MODIFY_REG(GPIOA->AFR[0],
+        GPIO_AFRL_AFSEL1 | GPIO_AFRL_AFSEL2 | GPIO_AFRL_AFSEL7,
+        (0b1011 << GPIO_AFRL_AFSEL1_Pos) | (0b1011 << GPIO_AFRL_AFSEL2_Pos) |
+        (0b1011 << GPIO_AFRL_AFSEL7_Pos));
+
+    // MODER = AF, AFR = AF11 for TX_EN, TXD0 and TXD1
+    MODIFY_REG(GPIOB->MODER,
+        GPIO_MODER_MODE11 | GPIO_MODER_MODE12 | GPIO_MODER_MODE13,
+        (0b10 << GPIO_MODER_MODE11_Pos) | (0b10 << GPIO_MODER_MODE12_Pos) |
+        (0b10 << GPIO_MODER_MODE13_Pos));
+    MODIFY_REG(GPIOB->AFR[1],
+        GPIO_AFRH_AFSEL11 | GPIO_AFRH_AFSEL12 | GPIO_AFRH_AFSEL13,
+        (0b1011 << GPIO_AFRH_AFSEL11_Pos) | (0b1011 << GPIO_AFRH_AFSEL12_Pos) |
+        (0b1011 << GPIO_AFRH_AFSEL13_Pos));
+    // OSPEED = 0b10 for TXD0 and TXD1
+    MODIFY_REG(GPIOB->OSPEEDR, GPIO_OSPEEDR_OSPEED12 | GPIO_OSPEEDR_OSPEED13,
+        (0b10 << GPIO_OSPEEDR_OSPEED12_Pos) | (0b10 << GPIO_OSPEEDR_OSPEED13_Pos));
+
+    // MODER = AF, AFR = AF11 for MDC, RXD0 and RXD1
+    MODIFY_REG(GPIOC->MODER,
+        GPIO_MODER_MODE1 | GPIO_MODER_MODE4 | GPIO_MODER_MODE5,
+        (0b10 << GPIO_MODER_MODE1_Pos) | (0b10 << GPIO_MODER_MODE4_Pos) |
+        (0b10 << GPIO_MODER_MODE5_Pos));
+    MODIFY_REG(GPIOC->AFR[0],
+        GPIO_AFRL_AFSEL1 | GPIO_AFRL_AFSEL4 | GPIO_AFRL_AFSEL5,
+        (0b1011 << GPIO_AFRL_AFSEL1_Pos) | (0b1011 << GPIO_AFRL_AFSEL4_Pos) |
+        (0b1011 << GPIO_AFRL_AFSEL5_Pos));
 }
 
 void setup_ethernet(void)
