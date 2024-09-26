eef63ceda5e685e0dcfea0b4793265e52635e526
vladubase <vladubase@gmail.com>
2024-09-26 19:35:19 +0300

    feat: add function master transmit, update def I2C_TIMINGR

--- include/i2c.h
+++ include/i2c.h
@@ -8,7 +8,14 @@
 
 #include "tools.h"
 
+// #define I2C_RISE_TIME   300
+// #define I2C_FALL_TIME   300
+// #define F_I2CCLK        9e6             // 9e6 - DNF=1, 8e6 - DNF=0
+// #define T_I2CCLK        1/F_I2CCLK
+#define I2C_TIMINGR     0x00B045E1      // 400kHz, From CubeMX.
+
 
 void setup_i2c(void);
+void i2c_master_transmit (uint8_t addr, const uint8_t * data, uint32_t num, bool xfer_pending);
 
 #endif	// #ifndef INCLUDE_I2C_H
